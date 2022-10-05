5e9f61703503501ca25bc93c5395139a6b493cbf
dipamato02 <dipamato@hotmail.com>
2022-10-05 08:49:02 -0500

    otros archivos

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HolaInicial
+{
+
+    public class DatosPersonales{
+        public string Nombre{get;set;}
+        public int Edad{get;set;}
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatosPersonales uno=new DatosPersonales();
+            uno.Nombre="Ramiro";
+            Console.Write("Ingrese la edad: ");
+            uno.Edad=int.Parse(Console.ReadLine());
+            Console.WriteLine(uno.Nombre +" tiene: "+uno.Edad);
+        }
+    }
+}
